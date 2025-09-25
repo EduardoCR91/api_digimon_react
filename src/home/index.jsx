6f590fd7c720d4f4f4828b6aba97bfe83bd3defb
Carlos Eduardo Cruz <121514453+EduardoCR91@users.noreply.github.com>
2025-09-25 02:52:12 +0000

    creacion de los componentes

---
+++ src/home/index.jsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+function Home(){
+    return(
+        <>
+        home
+        </>
+    )
+}
+
+export default Home;
---
+++ src/informativa/index.jsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+function Informativa(){
+    return(
+        <>
+        informativa
+        </>
+    )
+}
+
+export default Informativa;
